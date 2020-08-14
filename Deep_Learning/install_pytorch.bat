dbd7b9e23861460cfdc1cfe93c3ce38a073c7f76
ShiboXing <shx26@pitt.edu>
2020-08-14 15:51:29 +0800

    feat: add weight decay

--- Deep_Learning/install_pytorch.bat
+++ Deep_Learning/install_pytorch.bat
@@ -1,2 +1,3 @@
-call conda install pytorch torchvision cudatoolkit=10.2 torchtext -c pytorch 
-call conda install -c conda-forge jupyter matplotlib ipdb
+call conda install pytorch torchvision cudatoolkit=10.2 torchtext -c pytorch -y
+call conda install -c conda-forge jupyter matplotlib ipdb -y
+python -m ipykernel install --user --name base 
