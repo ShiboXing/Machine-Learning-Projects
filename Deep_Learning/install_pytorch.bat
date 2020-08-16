dc4768aaea0b6e05cc4a41c1c5cc8a0a6b2405cf
ShiboXing <shx26@pitt.edu>
2020-08-16 21:43:46 +0800

    feat: add HousePrice, Dropout

--- Deep_Learning/install_pytorch.bat
+++ Deep_Learning/install_pytorch.bat
@@ -1,3 +1,4 @@
 call conda install pytorch torchvision cudatoolkit=10.2 torchtext -c pytorch -y
 call conda install -c conda-forge jupyter matplotlib ipdb -y
+pip install pandas -y
 python -m ipykernel install --user --name base 
